2106bb2b85fd4927cfa1ea80633f366e4310668a
vadzimavich <vadzimavich@gmail.com>
2023-12-26 00:11:54 +0300

    feat: add some

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -570,8 +570,9 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  const r = Math.random() * (max - min) + min;
+  return r.toFixed(0);
 }
 
 /**
@@ -584,8 +585,8 @@ function getRandomInteger(/* min, max */) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -601,8 +602,12 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let n = 0;
+  for (let i = 0; i <= Math.abs(number); i += 1) {
+    n += i % 2 !== 0;
+  }
+  return n;
 }
 
 module.exports = {
